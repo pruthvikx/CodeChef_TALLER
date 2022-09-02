552c16ee600c92461a35cdfa74df94006603a3d2
Pruthvi <pruthvikx121@gmail.com>
2022-09-02 21:04:41 +0530

    codechef TALLER challenge

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scan = new Scanner(System.in);
+        int testCase = scan.nextInt();
+
+        for (int i = 0;i < testCase;i++){
+            System.out.println(scan.nextInt() < scan.nextInt() ? "B":"A");
+        }
+    }
+}
